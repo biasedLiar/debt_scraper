62995a1cd1555004c102ee186a26a2d4475bb068
biasedLiar <elelmtg@gmail.com>
2025-12-09 15:23:17 +0100

    fixed utilities

--- src/utilies.mjs
+++ src/utilies.mjs
@@ -3,14 +3,11 @@
  * @returns {boolean}
  */
 
-
 const fs = require("fs");
 
-
 export const savePage = (pageName) => {
-    const unsavedPages = ["bankid", "id-porten"];
-    console.log("'", unsavedPages[0], "'", " = '", pageName, "' --> ", (unsavedPages[0] === pageName));
-  if (unsavedPages.includes(pageName)){
+  const unsavedPages = ["bankid", "id-porten"];
+  if (unsavedPages.includes(pageName)) {
     return false;
   }
   return true;
@@ -21,28 +18,29 @@ export const savePage = (pageName) => {
  * @returns {string}
  */
 export const createFoldersAndGetName = (pageName) => {
-    var dateObj  = new Date();
-    const month   = (dateObj.getUTCMonth() + 1).toString().padStart(2,"0");;
-    const day     = dateObj.getUTCDate().toString().padStart(2,"0");
-    const year    = dateObj.getUTCFullYear();
+  var dateObj = new Date();
+  const month = (dateObj.getUTCMonth() + 1).toString().padStart(2, "0");
+  const day = dateObj.getUTCDate().toString().padStart(2, "0");
+  const year = dateObj.getUTCFullYear();
 
-    if (!pageName){
-        pageName = "no_page_name";
-    }
+  if (!pageName) {
+    pageName = "no_page_name";
+  }
 
-    const newDate = year + "_" + month + "_" + day;
-    
-    if (!fs.existsSync("./exports")){
-        fs.mkdirSync("./exports");
-    }
+  const newDate = year + "_" + month + "_" + day;
 
-    if (!fs.existsSync("./exports/" + newDate)){
-        fs.mkdirSync("./exports/" + newDate);
-    }
+  if (!fs.existsSync("./exports")) {
+    fs.mkdirSync("./exports");
+  }
 
-    if (!fs.existsSync("./exports/" + newDate + "/" + pageName)){
-        fs.mkdirSync("./exports/" + newDate + "/" + pageName);
-    }
-    const filename = "./exports/" + newDate + "/" + pageName + "/" + dateObj.getTime() + ".json";
+  if (!fs.existsSync("./exports/" + newDate)) {
+    fs.mkdirSync("./exports/" + newDate);
+  }
+
+  if (!fs.existsSync("./exports/" + newDate + "/" + pageName)) {
+    fs.mkdirSync("./exports/" + newDate + "/" + pageName);
+  }
+  const filename =
+    "./exports/" + newDate + "/" + pageName + "/" + dateObj.getTime() + ".json";
   return filename;
 };
