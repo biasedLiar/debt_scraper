fc442d613bc81f8795fc2e6b358b037cf4b7452f
Haakon Kanter <haakon.kanter@capgemini.com>
2025-12-16 12:56:07 +0100

    reverted bankid due to bug

--- src/pages/bankid-login.mjs
+++ src/pages/bankid-login.mjs
@@ -6,35 +6,34 @@
 export async function loginWithBankID(page, nationalID) {
   // Wait for and click the BankID link
   try {
-    await page.waitForSelector('a[href="/authorize/bankid"]');
+    await page.waitForSelector('a[href="/authorize/bankid"]', { timeout: 2000 });
     await page.click('a[href="/authorize/bankid"]');
-    console.log("Clicked BankID link");
+    console.log('Clicked BankID link');
   } catch (e) {
-    console.error("Could not find/click BankID link:", e);
+    console.error('Could not find/click BankID link:', e);
   }
-
+  
   //Needs to be optimized, improvised solution for now
-  await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for navigation
-
+  await new Promise(resolve => setTimeout(resolve, 5000)); // wait for navigation
+  
   // Wait for and fill in the national identity number input
   try {
     if (nationalID) {
-      await page.waitForSelector("input#nnin");
-      await page.type("input#nnin", nationalID);
-      console.log("Filled in national identity number");
+      await page.type('input#nnin', nationalID);
+      console.log('Filled in national identity number');
     } else {
-      console.log("No value to enter in national identity number field");
+      console.log('No value to enter in national identity number field');
     }
   } catch (e) {
-    console.error("Could not find/fill national identity number input:", e);
+    console.error('Could not find/fill national identity number input:', e);
   }
-
+  
   // Wait for and click the continue button
   try {
-    await page.waitForSelector("button#nnin-next-button");
-    await page.click("button#nnin-next-button");
-    console.log("Clicked continue button");
+    await page.waitForSelector('button#nnin-next-button', { timeout: 5000 });
+    await page.click('button#nnin-next-button');
+    console.log('Clicked continue button');
   } catch (e) {
-    console.error("Could not find/click continue button:", e);
+    console.error('Could not find/click continue button:', e);
   }
 }
