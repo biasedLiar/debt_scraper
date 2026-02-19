921384d803230e5f70eaac0e89a7dcade1af2a00
biasedLiar <elelmtg@gmail.com>
2026-02-19 13:57:08 +0100

    Added comment to intrum timeout length

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -9,7 +9,7 @@
 export const HANDLER_TIMEOUT_MS = 300000; // 300 seconds
 
 // Intrum visits each site and extracts data sequentially, so needs a longer timeout to accommodate the full process
-export const INTRUM_HANDLER_TIMEOUT_MS = 3000000; 
+export const INTRUM_HANDLER_TIMEOUT_MS = 3000000; // 3000 seconds (50 minutes) - Intrum can take a long time to load and extract data, especially for users with many cases 
 
 /**
  * File download polling configuration
